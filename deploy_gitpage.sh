a4314a851b1ddd6b0aed92489eed53444fa1f3f0
GuHaijun <243297288@qq.com>
2019-02-11 19:16:00 +0800

    #add deploy_gitpage.sh

---
+++ deploy_gitpage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/dartchina/dartchina.github.io.git site
+git config user.name "HaiJun Gu"
+git config user.email "243297288@qq.com"
+cd site && rm -rf * && cp -r ../_site/* .
+touch CNAME
+echo "www.dartdoc.cn" >> CNAME
+git add --all .
+git commit -m "Travis CI Auto Builder"
+git push --force https://$DEPLOY_TOKEN@github.com/dartchina/dartchina.github.io.git master
